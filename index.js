eedefa6d8c45c1a5e47a5aa2b86112429784edd7
GMchaudry <g.m.chaudry@gmailc.om>
2024-04-17 10:28:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+//called shebang ,to publish it on npm 
+import inquirer from "inquirer";
+// to enquire from user
+let answer = await inquirer.prompt({
+    /*let answer:{
+        sentence:string
+    }=await inquirer.prompt({*/
+    name: "sentence",
+    type: "input",
+    message: "Enter your sentence for word count:"
+});
+// this will remove the white spaces
+let word = answer.sentence.trim().split(" ");
+// this will array the log in the console
+console.log(word);
+// this will count the words in console
+console.log("\n The total word in this sentence is : \n");
+console.log(`${word.length}`);
